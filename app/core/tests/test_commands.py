9a06c20ad07cdd63dc126d18b86d091e4bd1b35c
PuneetSivananda <psivananda@chatsoft.com>
2020-10-06 11:32:50 +0530

    feat: updated postgres startup test

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -1,7 +1,7 @@
 from unittest.mock import patch
 
 from django.core.management import call_command
-from djando.db.utils import OperationalError
+from django.db.utils import OperationalError
 from django.test import TestCase
 
 
