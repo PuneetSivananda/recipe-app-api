d5cee0e9edaab6a7387bd0b3a2b8f50226a26d11
PuneetSivananda <psivananda@chatsoft.com>
2021-02-28 19:34:28 +0530

    chore: formatting

--- app/receipe/tests/test_receipe_api.py
+++ app/receipe/tests/test_receipe_api.py
@@ -10,34 +10,38 @@ from receipe.serializers import RecipeSerializer
 
 RECEIPES_URL = reverse('receipe:receipe-list')
 
-def sample_recipe(user, **params):
-  """Create and return a sample receipe"""
-  defaults = {
-    "title":"Sample Receipe",
-    "time_minutes": 10,
-    "price":5
-  }
-  defaults.update(params)
 
-  return receipe.objects.create(user = user, **defaults)
+def sample_recipe(user, **params):
+    """Create and return a sample receipe"""
+    defaults = {
+        "title": "Sample Receipe",
+        "time_minutes": 10,
+        "price": 5
+    }
+    defaults.update(params)
+
+    return receipe.objects.create(user=user, **defaults)
+
 
 class PublicReceipeApiTests(TestCase):
-  """Test Un authorized receipe API access"""
-  def setup(self):
-    self.client = APIClient()
+    """Test Un authorized receipe API access"""
 
-  def test_auth_required(self):
-    """Test that the authentication is required"""
-    res = self.client.get(RECEIPES_URL)
-    self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
+    def setup(self):
+        self.client = APIClient()
+
+    def test_auth_required(self):
+        """Test that the authentication is required"""
+        res = self.client.get(RECEIPES_URL)
+        self.assertEqual(res.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
 class PrivateReceipeApiTests(TestCase):
-  """Test Unauthorized receipe api access"""
-  def setup(self):
-    self.client = APIClient()
-    self.user = get_user_model().objects.create_user(
-      'test@testuser.com',
-      'testpass'
-    )
-    self.client.force_authenticate(self.user)
+    """Test Unauthorized receipe api access"""
+
+    def setup(self):
+        self.client = APIClient()
+        self.user = get_user_model().objects.create_user(
+            'test@testuser.com',
+            'testpass'
+        )
+        self.client.force_authenticate(self.user)
